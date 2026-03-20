14653dd418f731663566ce3eb5df51832dfe4a4e
Bimal Kunwar <bimalknwr3@gmail.com>
2026-03-20 20:30:39 +0545

    backend and project setuppp

---
+++ backend/server.js
@@ -0,0 +1,59 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import connectDB from './config/db.js';
+import errorHandler from './middleware/errorHandler.js';
+// import { error } from 'console';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Express app initialize gareko
+const app = express();
+
+//mongodb sanga connect gareko 
+connectDB();
+
+//CORS handle garna middleware use gareko
+app.use(
+    cors({
+        origin:"*",
+        methods:["GET","POST","PUT","DELETE"],
+        allowedHeaders:["Content-Type","Authorization"],
+        credentials:true
+    }));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//statuc folder upload ko lagi
+app.use('/uploads',express.static(path.join(__dirname,'uploads')));
+
+// Routes
+app.use(errorHandler);
+
+ //404 error handle garna middleware
+app.use((req,res,next)=>{
+    res.status(404).json({
+        success:false,
+        error:"Route not found",
+        statusCode:404
+        });
+});
+
+//  server start garna
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on('unhandledRejection',(err)=>{
+    console.log(`Error: ${err.message}`);
+    // console.log('Shutting down the server due to unhandled promise rejection');
+    process.exit(1);
+});
